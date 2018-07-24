97acf2d7dcf25fa0a51e492c9ad86a4fd3156402
Luis Ruiz <luis.alonso.16@hotmail.com>
2018-07-24 03:08:52 -0600

    Add All Files

---
+++ Code/MVC classes/ClsContacto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class ClsContacto
+    { 
+        //opcion a ejecutar en el SP
+        public int opc { get; set; }
+
+
+        // Propiedades de la clase
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+    }
+}
